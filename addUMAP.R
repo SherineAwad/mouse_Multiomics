6c4bdede93967d870cfd28c8bb246330bdecc2a7
Sherine Awad <sherinem@gl3199.arc-ts.umich.edu>
2025-05-20 15:12:36 -0400

    update dimToUse in addUmap

--- addUMAP.R
+++ addUMAP.R
@@ -58,13 +58,13 @@ proj_ALL <- addIterativeLSI(
 #proj_ALL <- addCombinedDims(proj_ALL, reducedDims = c("LSI_ATAC", "LSI_RNA"), name =  "LSI_Combined", force =TRUE)
 #proj_ALL <- addUMAP(proj_ALL, reducedDims = "LSI_ATAC", name = "UMAP_ATAC", minDist = 0.8, force = TRUE)
 #proj_ALL <- addUMAP(proj_ALL, reducedDims = "LSI_RNA", name = "UMAP_RNA", minDist = 0.8, force = TRUE)
-proj_ALL <- addUMAP(proj_ALL, reducedDims = "LSI_Combined", dimsToUse = 1:80, name = "UMAP_Combined", minDist = 0.4, force = TRUE)
+proj_ALL <- addUMAP(proj_ALL, reducedDims = "LSI_Combined", dimsToUse = 1:55, name = "UMAP_Combined", minDist = 0.4, force = TRUE)
 #-----------------------------
 #Add Clusters
 #----------------------------
 proj_ALL <- addClusters(proj_ALL, reducedDims = "LSI_ATAC", name = "Clusters_ATAC", resolution = 0.6, force = TRUE)
 proj_ALL <- addClusters(proj_ALL, reducedDims = "LSI_RNA", name = "Clusters_RNA", resolution = 0.6, force = TRUE)
-proj_ALL <- addClusters(proj_ALL, reducedDims = "LSI_Combined",dimsToUse = 1:20, name = "Clusters_Combined", resolution = 1.2, force = TRUE)
+proj_ALL <- addClusters(proj_ALL, reducedDims = "LSI_Combined",dimsToUse = 1:30, name = "Clusters_Combined", resolution = 1.2, force = TRUE)
 
 figure_name <- project_name
 figure_name <- paste(figure_name,"_perClustersnUMI.pdf", sep="")
